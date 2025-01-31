68d09b94ea9eea5e3334bfa165f09f0dd1d79ae3
Ibraim Barrios <barrios.party@hotmail.com>
2025-01-30 22:10:23 -0600

    Script generado en base a types.ts

--- dist/types.js
+++ dist/types.js
@@ -1,1 +1,30 @@
-console.log("hola");
+//tipos(o tipados) pricipales de datos son:
+var age = "hello";
+var agMonths = 20;
+var isSenior = true;
+var person = {};
+// ejemplos de listas en typeScript
+var fruits = ["uva", "pera"];
+var persons = [
+    { name: "platano", color: "amarillo" },
+    { name: "uva", color: "verde" },
+];
+// tipos especiales de datos en typeScript
+// Nota: any asepta todo tipo de dato y puede Re-asicnarce, ejemplo:
+var response = "hello";
+response = 20;
+response = true;
+response = ["hola", 123];
+// void, ejecuta cosas pero no retorna nada
+function hello() {
+    console.log("Hello");
+}
+// unknown, similara any pero mas controlado
+var response2;
+response2 = true;
+if (response2) {
+    console.log("here");
+}
+// tipos de datos basios
+var response3 = null;
+var response4 = undefined;
